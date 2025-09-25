004037ca619c3cc64367d41afaa48ef7da3ec6b4
dkwa0000 <joakim.tubring@gmail.com>
2025-09-25 19:12:28 +0200

    Adds basic version of Post.java

---
+++ src/toxic/Post.java
@@ -0,0 +1,61 @@
+package toxic;
+
+// Reportable interface
+// + report()
+// Post implements Reportable, Likable
+
+// + user, msg, timestamp, likers, reported [reportList (reported, moderated)]
+
+import toxic.common.Likeable;
+import toxic.common.Reportable;
+import toxic.user.*;
+
+import java.util.ArrayList;
+
+public class Post implements Likeable, Reportable {
+
+    // + user,
+    // msg,
+    // timestamp,
+    // likers,
+    // reported
+
+    private User user;
+    private String msg;
+    private ArrayList<User> likers;
+    private boolean reported;
+
+    public Post(User user, String msg){
+        this.user = user;
+        this.msg = msg;
+        this.likers = new ArrayList<>();
+    }
+
+    @Override
+    public void unLike(User user) {
+        if(this.likers.contains(user)){
+            this.likers.remove(user);
+        }
+    }
+
+    @Override
+    public void like(User user) {
+        if(!this.likers.contains(user)){
+            this.likers.add(user);
+        }
+    }
+
+    @Override
+    public void report(User user, String msg) {
+        this.reported = true;
+    }
+
+    public ArrayList<User> getLikes(){ return this.likers; }
+
+    // [reportList (reported, moderated)]
+
+    //List<Reported> list = (reported(msg, User), moderated(null eller placeHolder))
+
+    //list.add(list.get(list.size - 1).reported);
+
+}
