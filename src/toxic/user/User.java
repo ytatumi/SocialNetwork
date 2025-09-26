526c1cafa80e43523987a5e883c11b2e9f0617d7
AnthonyRG <anthonyrizk89@gmail.com>
2025-09-26 12:29:20 +0200

    added User

---
+++ src/toxic/user/User.java
@@ -0,0 +1,14 @@
+public abstract class User {
+ String name;
+ String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    public Post createPost(String msg) {
+    }
+    public Post reportPost(Post post) {
+    }
+
+ }
