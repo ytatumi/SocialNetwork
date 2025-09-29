a4648e44ae6e9dbfdd78f8076181331ff8b51368
Begaiym Kalilova <easybeka@gmail.com>
2025-09-29 14:43:14 +0200

    add admin and usermanagement

---
+++ src/toxic/management/UserManagement.java
@@ -0,0 +1,40 @@
+package toxic.management;
+
+import toxic.user.RegularUser;
+import toxic.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserManagement {
+
+    private static volatile UserManagement instance;
+    List<User> userList = new ArrayList<>();
+
+    private UserManagement() {
+        generateDefaultUser();
+    }
+
+    private void generateDefaultUser() {
+        User regularUser1 = new RegularUser("Test User", "test.google.com");
+        this.userList.add(regularUser1);
+    }
+
+    public static UserManagement getInstance() {
+        if (instance == null) {
+            synchronized (UserManagement.class) {
+                if (instance == null) {
+                    instance = new UserManagement();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+    public void updateUserList(List<User> userList) {
+        this.userList = userList;
+    }
+}
--- src/toxic/user/AdminUser.java
+++ src/toxic/user/AdminUser.java
@@ -1,26 +1,41 @@
 package toxic.user;
 
-import toxic.user.User;
-
-import toxic.Post;
+import toxic.management.UserManagement;
 
 import java.util.List;
 
-public class AdminUser extends User implements Administration, Moderation{
-    List<Post> ReportedPosts;
-    List<Post> listOfPosts;
+public class AdminUser extends User implements Administration, Moderation {
 
     public AdminUser(String name, String email) {
         super(name, email);
     }
 
+
     @Override
     public void deleteUser(User user) {
+        List<User> userList = UserManagement.getInstance().getUserList();
 
+        if(user instanceof RegularUser){
+            userList.remove(user);
+            System.out.println("Admin " + this.name + " has deleted " + user.getName());
+        }
+        UserManagement.getInstance().updateUserList(userList);
     }
 
     @Override
     public void banUser(User user) {
+        List<User> userList = UserManagement.getInstance().getUserList();
+
+        if(user instanceof RegularUser) {
+            for (User u : userList) {
+                if (u.equals(user)) {
+                    u.setBanned(true);
+                    break;
+                }
+            }
+            System.out.println("Admin " + this.name + " has banned " + user.getName());
+        }
+        UserManagement.getInstance().updateUserList(userList);
 
     }
 
