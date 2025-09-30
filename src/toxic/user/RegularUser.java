14be13b2026a5e0dd63db177ad21cca77eb3dc78
AnthonyRG <anthonyrizk89@gmail.com>
2025-09-30 15:38:30 +0200

    RegularUser implements Reportable

--- src/toxic/user/RegularUser.java
+++ src/toxic/user/RegularUser.java
@@ -1,7 +1,20 @@
 package toxic.user;
 
-public class RegularUser extends User{
+import toxic.Report;
+import toxic.common.Reportable;
+import java.util.List;
+
+public class RegularUser extends User implements Reportable {
+    boolean reported;
+    private List<Report> reports;
     public RegularUser(String name, String email){
         super(name, email);
+    }
+
+
+    @Override
+    public void report(User user, String msg) {
+        reported = true;
+        reports.add(new Report(this, "<<USER>>"));
     }
 }
